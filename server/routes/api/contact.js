6f185de00a962e03a5d59e5acd2e2e595b6f8e46
ben <ben@surgison.net>
2019-05-23 21:49:34 +0100

    Add contact

--- server/routes/api/contact.js
+++ server/routes/api/contact.js
@@ -5,46 +5,56 @@ module.exports = [
   {
     method: 'GET',
     path: '/contacts',
-    handler: () => {
-      return Contact.findAll()
-    },
-    tags: ['api']
+    options: {
+      handler: () => {
+        return Contact.findAll()
+      },
+      tags: ['api']
+    }
   }, {
     method: 'GET',
     // The curly braces are how we define params (variable path segments in the URL)
     path: '/contacts/{id}',
-    handler: (request) => {
-      return Contact.findById(request.params.id)
-    },
-    tags: ['api']
+    options: {
+      handler: (request) => {
+        return Contact.findById(request.params.id)
+      },
+      tags: ['api']
+    }
   }, {
     method: 'POST',
     path: '/contacts',
-    handler: (request) => {
-      const contact = Contact.build(request.payload.contact)
+    options: {
+      handler: (request) => {
+        const contact = Contact.build(request.payload.contact)
 
-      return contact.save()
-    },
-    tags: ['api']
+        return contact.save()
+      },
+      tags: ['api']
+    }
   }, {
     // method can be an array
     method: ['PUT', 'PATCH'],
     path: '/contacts/{id}',
-    handler: async (request) => {
-      const contact = await Contact.findById(request.params.id)
-      contact.update(request.payload.contact)
+    options: {
+      handler: async (request) => {
+        const contact = await Contact.findById(request.params.id)
+        contact.update(request.payload.contact)
 
-      return contact.save()
-    },
-    tags: ['api']
+        return contact.save()
+      },
+      tags: ['api']
+    }
   }, {
     method: 'DELETE',
     path: '/contacts/{id}',
-    handler: async (request) => {
-      const contact = await Contact.findById(request.params.id)
+    options: {
+      handler: async (request) => {
+        const contact = await Contact.findById(request.params.id)
 
-      return contact.destroy()
-    },
-    tags: ['api']
+        return contact.destroy()
+      },
+      tags: ['api']
+    }
   }
 ]
