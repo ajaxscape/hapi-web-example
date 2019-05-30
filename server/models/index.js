abd1345769d8291b76d9541cdde099f4e6a0a071
ben <ben@ajaxscape.com>
2019-05-30 16:23:10 +0100

    Require all models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,8 @@
-module.exports = {
-  Application: require('./application.model'),
-  Contact: require('./contact.model')
-}
+const requireAll = require('require-all')
+
+// Requires all the models in the current folder
+module.exports = requireAll({
+  dirname: __dirname,
+  filter: /^(.+).model\.js$/,
+  map: (name) => name.charAt(0).toUpperCase() + name.slice(1)
+})
