975d318a8011ba9b3c7fd68c267b1679081ccec8
ben <ben@surgison.net>
2019-06-03 11:04:55 +0100

    Ignore logs

--- server/plugins/logging.js
+++ server/plugins/logging.js
@@ -1,28 +1,49 @@
+const config = require('../config')
+
+const squeezeConfig = [{
+  'error': '*',
+  'log': '*',
+  'ops': '*',
+  'request': '*',
+  'response': '*'
+}]
+
 module.exports = {
   plugin: require('good'),
   options: {
     ops: {
-      interval: 1000
+      // Log ops stats every 30 seconds
+      interval: 30000
     },
     reporters: {
-      console: [
+      // Output to console
+      consoleReporter: [
         {
           module: 'good-squeeze',
           name: 'Squeeze',
+          args: squeezeConfig
+        }, {
+          module: 'good-console',
           args: [
             {
-              log: '*',
-              error: '*',
-              response: '*',
-              request: '*'
-            }
-          ]
-        },
+              format: 'YYYY-MM-DD HH:mm:ss',
+              utc: false
+            }]
+        }, 'stdout'],
+
+      // Output to file
+      fileReporter: [
         {
-          module: 'good-console'
-        },
-        'stdout'
-      ]
+          module: 'good-squeeze',
+          name: 'Squeeze',
+          args: squeezeConfig
+        }, {
+          module: 'good-squeeze',
+          name: 'SafeJson'
+        }, {
+          module: 'good-file',
+          args: ['./log/' + config.env + '.log']
+        }]
     }
   }
 }
