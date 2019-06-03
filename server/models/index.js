43bddb87efe81d2c4c2c05a0060b53b5917a86be
ben <ben@surgison.net>
2019-06-03 09:02:41 +0100

    Add comments

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const requireAll = require('require-all')
 
 // Requires all the models in the current folder
+// eg. when the models directory contains files /application.model.js and /contact.model.js they will be exported as an array of classes [ Application, Contact ]
 module.exports = requireAll({
   dirname: __dirname,
   filter: /^(.+).model\.js$/,
